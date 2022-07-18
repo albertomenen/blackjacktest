155c41d143b6c6cc13100e6939ba42b5e853335b
Alberto Menendez <90271858+albertomenen@users.noreply.github.com>
2022-07-18 23:52:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+
+
+import random
+
+def deal_card():
+  """Devolver una carta random"""
+  cards=[11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
+
+def calculate_score (cards):
+
+
+   if sum(cards) == 21 and len(cards) == 2:
+    return 0
+    if 11 in cards and sum(cards) > 21:
+      cards.remove(11)
+      cards.append(1)
+
+    return sum(cards)
+
+
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
+
+while not is_game_over:
+
+
+  user_score = calculate_score(user_cards)
+  computer_score = calculate_score(computer_cards)
+  print(f"Tus cartas: {user_cards}, puntuacion: {user_score} ")
+  print(f" primera carta del ordenador: {computer_cards[0]} ")
+
+
+  if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+  else:
+    user_should_deal =input("Teclea 's'  para obtener otra carta, teclea 'n' para pasar")
+    if user_should_deal == "s":
+      user_cards.append(deal_card())
+    else:
+      is_game_over = True
+
+
+while computer_score != 0 and computer_score < 17:
+  computer_cards.append(deal_card())
+  computer_score = calculate_score(computer_cards)
